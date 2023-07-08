19fba03a2a3d5fe5ac6c4811aa72586c000e1aae
Cedric <yacmov@gmail.com>
2023-07-08 21:43:59 +0800

    02_04-Variable

---
+++ 01_Learning_Python/02_04-Variable.py
@@ -0,0 +1,10 @@
+animal = "Dog"
+name = "Happy"
+hobby = "go to park"
+age = 4
+is_adult = age >= 3
+
+print(f"My {animal}'s name is {name}.")
+print(f"{name}'s age is {age} and love {hobby}.")
+print(f"Is {name} an adult? [{is_adult}]")
+
